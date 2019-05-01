a741eacff907489f767da0d733518d3efefe747c
brian7346 <doctorbrian97@gmail.com>
2019-05-01 11:57:58 +0400

    Ititial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://louis:louis1488@ds149606.mlab.com:49606/devconnector"
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// @route  GET api/prolile/test
+// @desc   Tests post route
+// @access Public
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+module.exports = router;
