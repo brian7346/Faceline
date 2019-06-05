50a2187cbd88673285d9d4b56391bfff6827f6ba
brian7346 <doctorbrian97@gmail.com>
2019-06-05 13:23:35 +0400

    Profile

---
+++ models/Profile.js
@@ -0,0 +1,115 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create Schema
+// Схема для Профиля пользователя
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skils: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  exprience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      compamy: {
+        type: String,
+        require: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        require: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        require: true
+      },
+      fieldofstudy: {
+        type: String,
+        require: true
+      },
+      from: {
+        type: Date,
+        require: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    twitter: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
