cbe35b56cd9406c071cd9da1d3461ee6e9640ec5
maxroslow <brian2009@yandex.ru>
2019-07-05 12:11:35 +0400

    Prepare for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://louis:louis1488@ds149606.mlab.com:49606/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
