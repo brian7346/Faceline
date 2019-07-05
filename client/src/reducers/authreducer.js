8975a4c5df433e487a7a04854bc50888c4a0e6ea
maxroslow <brian2009@yandex.ru>
2019-07-05 18:16:18 +0400

    Reducer path

--- client/src/reducers/authreducer.js
+++ client/src/reducers/authreducer.js
@@ -1,7 +1,7 @@
 import { SET_CURRENT_USER } from "../actions/types";
 import { isEmpty } from "../validation/is-empty";
 
-export let authReducer = (state, action) => {
+let authReducer = (state, action) => {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
@@ -13,3 +13,5 @@ export let authReducer = (state, action) => {
       return state;
   }
 };
+
+export default authReducer;
