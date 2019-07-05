84176ae21f2a71565515f25194d31a456fe06418
maxroslow <brian2009@yandex.ru>
2019-07-05 12:54:53 +0400

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -10,6 +11,7 @@ const posts = require("./routes/api/posts");
 const app = express();
 
 // DB config
+// Конфиг для монго ДБ
 const db = require("./config/keys").mongoURI;
 
 //Body Parser Middleware
@@ -32,6 +34,18 @@ require("./config/passport.js")(passport);
 app.use("/api/users/", users);
 app.use("/api/profile/", profile);
 app.use("/api/posts/", posts);
+
+// Server static assets if in production
+// Точка входа, если продакшн
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  // Указываем папку, в которой нужно искать
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
