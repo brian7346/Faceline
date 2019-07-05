84b0a56b0304729937b585aaf6f7f73cc2d5f105
maxroslow <brian2009@yandex.ru>
2019-07-05 18:14:17 +0400

    Root file for reducers

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,25 +1,9 @@
 import React, { useReducer } from "react";
-// import { authReducer, initialState } from "../reducers/authReducer";
-
-import { SET_CURRENT_USER } from "../actions/types";
-import { isEmpty } from "../validation/is-empty";
+import { authReducer } from "../reducers";
 
 let initialState = {
   isAuthenticated: false,
   user: {}
-};
-
-let authReducer = (state, action) => {
-  switch (action.type) {
-    case SET_CURRENT_USER:
-      return {
-        ...state,
-        isAuthenticated: !isEmpty(action.payload),
-        user: action.payload
-      };
-    default:
-      return state;
-  }
 };
 
 let AuthContext = React.createContext();
--- client/src/reducers/authreducer.js
+++ client/src/reducers/authreducer.js
@@ -1,11 +1,6 @@
 import { SET_CURRENT_USER } from "../actions/types";
 import { isEmpty } from "../validation/is-empty";
 
-export let initialState = {
-  isAuthenticated: false,
-  user: {}
-};
-
 export let authReducer = (state, action) => {
   switch (action.type) {
     case SET_CURRENT_USER:
